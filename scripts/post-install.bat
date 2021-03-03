ddc482898baee1bb8e10c3c863e5e4f7fe0dceff
heetbeet <example@example.com>
2021-03-03 16:32:59 +0200

    Fixed broken xlwings setup reference

--- scripts/post-install.bat
+++ scripts/post-install.bat
@@ -1,3 +1,3 @@
 :: This file will run on the user's system right after the installation is complete
 
-@call "%~dp0\Add xlwings to Excel.bat" dont-pause
+@call "%~dp0\..\src\Add xlwings to Excel.bat" dont-pause
