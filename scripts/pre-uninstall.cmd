09a16331ca773ab498f27c73af828c7f25a77a5c
heetbeet <example@example.com>
2021-03-13 18:28:44 +0200

    Remove xlwings when uninstalling

---
+++ scripts/pre-uninstall.cmd
@@ -0,0 +1,3 @@
+:: This file will run on the user's system right after the installation is complete
+
+@call "%~dp0\..\src\Remove xlwings from Excel.cmd" dont-pause
--- src/Remove xlwings from Excel.cmd
+++ src/Remove xlwings from Excel.cmd
@@ -1,3 +1,6 @@
 @call "%~dp0\activate-pythonva-environment.cmd"
 @call "%python_home%\scripts\xlwings" addin remove
-@pause
+
+@if "%1" equ "" (
+    @pause
+)
